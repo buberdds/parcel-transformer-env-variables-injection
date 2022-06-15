9f30e0043c43eb0d81e511ff0423272b033e6a3f
Luka Jeran <lukaw3d@gmail.com>
2022-06-16 00:11:16 +0200

    Invalidate cache when env vars change

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const { Transformer } = require('@parcel/plugin');
 module.exports = new Transformer({
   async transform({ asset, options: { env } }) {
     const code = await asset.getCode();
-    const result = code.replace(/{{{ ([^}]+) }}}/g, (match, envKey) =>
-      env[envKey] === undefined ? match : env[envKey]
-    );
+    const result = code.replace(/{{{ ([^}]+) }}}/g, (match, envKey) => {
+      asset.invalidateOnEnvChange(envKey);
+      return env[envKey] === undefined ? match : env[envKey];
+    });
     asset.setCode(result);
     return [asset];
   },
